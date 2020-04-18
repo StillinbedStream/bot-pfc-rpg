4167e4ce3ff1f60d6279278b14aa7a4fdc7fef40
StillinBed <stillinbedstream@gmail.com>
2020-04-18 21:07:37 +0200

    Ajout d'une API
    Une API qui permet de lire les données et de rendre accessibles certaines informations. Ici, les premiers joueurs du classement.

---
+++ pfc-api.py
@@ -0,0 +1,15 @@
+
+# Here is the api flask server runner for PFC game.
+
+from flask import Flask
+from flask import render_template
+import pickle
+import os.path
+
+app = Flask(__name__)
+
+@app.route('/')
+def first_players():
+    with open("data.cornichon", "rb") as f:
+        data = pickle.load(f)
+        return render_template("first_players.html", players=data.ranking[:5])
